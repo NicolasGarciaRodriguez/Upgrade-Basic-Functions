d1e51a3e8b07103f001bc48745f0df78f02edf48
NicoGarcia <nicogarcia.ro97@gmail.com>
2021-10-22 09:41:20 +0200

    Calcular la suma

--- Functions.js
+++ Functions.js
@@ -1,9 +1,29 @@
 // 1. Buscar el máximo
 
-function sum(numberOne , numberTwo) {
-    if (numberOne > numberTwo) {
-        console.log(numberOne)
-    } else {
-        console.log(numberTwo)
-    }
-  }
+// function sum(numberOne , numberTwo) {
+//     if (numberOne > numberTwo) {
+//         console.log(numberOne)
+//     } else {
+//         console.log(numberTwo)
+//     }
+//   }
+
+
+// 2. Buscar la palabra más larga
+
+
+
+
+// 3. Calcular la suma
+
+// let numbers = [1, 2, 3, 5, 45, 37, 58];
+// let total = 0
+// function sumAll(param) {
+//     for (let i = 0; i <= numbers.length; i++) {
+//         total += numbers[i];
+//         console.log(total)
+//     }
+//   }
+
+//   sumAll()
+
